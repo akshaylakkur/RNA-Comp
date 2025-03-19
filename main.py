3340c707a975103d7ee68934b98178b7c7129bdf
akshaylakkur <akshay.lakkur@gmail.com>
2025-03-19 12:28:23 -0700

    Updated Main

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
+from torch.utils.data import DataLoader, TensorDataset
+import os
 
 TRAIN_DATA_PATH = '/Users/akshaylakkur/PycharmProjects/RNAComp/RNA-Data-Sorted/'
 SEQUENCES_PATH = '/Users/akshaylakkur/PycharmProjects/RNAComp/stanford-rna-3d-folding/train_sequences.csv'
-SEQUENCES = pd.read_csv(SEQUENCES_PATH)['sequence'].unique()
-print(SEQUENCES, len(SEQUENCES))
+def apply_func_ids(string):
+    string = string.split('_')[0]
+    return string
+def apply_func_seqs(string):
+    string = string.split('-')[0]
+    return string
+IDS = list(pd.read_csv(SEQUENCES_PATH)['target_id'].apply(apply_func_ids))
+SEQUENCES = list(pd.read_csv(SEQUENCES_PATH)['sequence'].apply(apply_func_seqs).unique())
+print(IDS)
+'''
+model input -> Sequence, individual nucleotides
+'''
+def one_hot(sequence):
+    maps = {'A':1, 'C':2, 'G':3, 'U':4, 'X':0}
+    seqs = []
+    for i in sequence:
+        if i!='-':
+            seqs.append(maps[i])
+        else:
+            for j in range(len(sequence) - sequence.index(i) + 1):
+                seqs.append(0)
+    indices = torch.tensor(seqs)
+    one_hot_sqn = F.one_hot(indices, num_classes=5)
+    return one_hot_sqn, indices
 
-# scatter = go.Scatter3d(
-#     x=xs, y=ys, z=zs,
-#     mode='lines',
-#     marker=dict(
-#         size=5,
-#         color=zs,  # Color by z-value
-#         colorscale='Viridis',  # Choose a colorscale
-#         opacity=0.8
-#     )
-# )
-#
-# # Create the layout
-# layout = go.Layout(
-#     title='3D Scatter Plot',
-#     scene=dict(
-#         xaxis=dict(title='X-axis'),
-#         yaxis=dict(title='Y-axis'),
-#         zaxis=dict(title='Z-axis')
-#     )
-# )
-#
-# # Create the figure
-# fig = go.Figure(data=[scatter], layout=layout)
-#
-# # Show the plot
-# fig.show()
+def create_one_hot_plus_pad():
+    SEQUENCES_LIST = []
+    TRUTH = []
+    for i in SEQUENCES:
+        onehot, seqs = one_hot(i)
+        SEQUENCES_LIST.append(onehot)
+        TRUTH.append(seqs)
+    return SEQUENCES_LIST, TRUTH
+
+SEQUENCES_LIST, TRUTH = create_one_hot_plus_pad()
+x = pad_sequence(SEQUENCES_LIST, batch_first=True, padding_value=0)
+mask = x.dim() != 0
+dataset = TensorDataset(x, )
+class RNAModule(nn.Module):
+    def __init__(self):
+        super(RNAModule, self).__init__()
+
